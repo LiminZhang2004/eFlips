9d7bc7d251017ca32b27cf8d8dc683527e14d5a0
Limin Zhang <liminzhang@hotmail.com>
2014-04-02 15:06:24 -0500

    Changes made by Limin Zhang (liminzhang@hotmail.com).

--- eFlips/scripts/app.js
+++ eFlips/scripts/app.js
@@ -1,9 +1,15 @@
 (function (global) {
+    
     var mobileSkin = "",
         app = global.app = global.app || {};
 
     document.addEventListener('deviceready', function () {
         navigator.splashscreen.hide();
+        
+        cameraApp = new cameraApp();
+        cameraApp.run();
+	     
+        
     }, false);
 
     app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout"});
@@ -21,3 +27,108 @@
         app.application.skin(mobileSkin);
     };
 })(window);
+
+
+function id(element) {
+    return document.getElementById(element);
+}
+
+function cameraApp(){}
+
+cameraApp.prototype={
+    _pictureSource: null,
+    
+    _destinationType: null,
+    
+    run: function(){
+      
+	    
+        var that=this;
+	    that._pictureSource = navigator.camera.PictureSourceType;
+	    that._destinationType = navigator.camera.DestinationType;
+        
+	    id("capturePhotoButtonBefore").addEventListener("click", function(){
+            that._capturePhoto.apply(that,arguments);
+        });
+		id("capturePhotoButtonAfter").addEventListener("click", function(){
+            that._capturePhoto.apply(that,arguments);
+        });
+	    
+    },
+    
+    _capturePhoto: function() {
+        var that = this;
+        
+        // Take picture using device camera and retrieve image as base64-encoded string.
+        navigator.camera.getPicture(function(){
+            that._onPhotoDataSuccess.apply(that,arguments);
+        },function(){
+            that._onFail.apply(that,arguments);
+        },{
+            quality: 50,
+            destinationType: that._destinationType.DATA_URL
+        });
+    },
+    
+    _capturePhotoEdit: function() {
+        var that = this;
+        // Take picture using device camera, allow edit, and retrieve image as base64-encoded string. 
+        // The allowEdit property has no effect on Android devices.
+        navigator.camera.getPicture(function(){
+            that._onPhotoDataSuccess.apply(that,arguments);
+        }, function(){
+            that._onFail.apply(that,arguments);
+        }, {
+            quality: 20, allowEdit: true,
+            destinationType: cameraApp._destinationType.DATA_URL
+        });
+    },
+    
+    _getPhotoFromLibrary: function() {
+        var that= this;
+        // On Android devices, pictureSource.PHOTOLIBRARY and
+        // pictureSource.SAVEDPHOTOALBUM display the same photo album.
+        that._getPhoto(that._pictureSource.PHOTOLIBRARY);         
+    },
+    
+    _getPhotoFromAlbum: function() {
+        var that= this;
+        // On Android devices, pictureSource.PHOTOLIBRARY and
+        // pictureSource.SAVEDPHOTOALBUM display the same photo album.
+        that._getPhoto(that._pictureSource.SAVEDPHOTOALBUM)
+    },
+    
+    _getPhoto: function(source) {
+        var that = this;
+        // Retrieve image file location from specified source.
+        navigator.camera.getPicture(function(){
+            that._onPhotoURISuccess.apply(that,arguments);
+        }, function(){
+            cameraApp._onFail.apply(that,arguments);
+        }, {
+            quality: 50,
+            destinationType: cameraApp._destinationType.FILE_URI,
+            sourceType: source
+        });
+    },
+    
+    _onPhotoDataSuccess: function(imageData) {
+        var smallImage = document.getElementById('beforeImage');
+        smallImage.style.display = 'block';
+    
+        // Show the captured photo.
+        smallImage.src = "data:image/jpeg;base64," + imageData;
+    },
+    
+    _onPhotoURISuccess: function(imageURI) {
+        var smallImage = document.getElementById('smallImage');
+        smallImage.style.display = 'block';
+         
+        // Show the captured photo.
+        smallImage.src = imageURI;
+    },
+    
+    _onFail: function(message) {
+        alert(message);
+    }
+}
